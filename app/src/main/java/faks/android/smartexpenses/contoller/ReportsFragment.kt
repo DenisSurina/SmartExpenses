6d55383487d5e49f446c22c96980c3c42a367a02
Denis Surina <denis.surina1@gmail.com>
2024-01-15 23:48:59 +0100

    added pie chart

--- app/src/main/java/faks/android/smartexpenses/contoller/ReportsFragment.kt
+++ app/src/main/java/faks/android/smartexpenses/contoller/ReportsFragment.kt
@@ -1,6 +1,7 @@
 package faks.android.smartexpenses.contoller
 
 import android.content.res.ColorStateList
+import android.graphics.Color
 import android.graphics.Paint
 import android.os.Bundle
 import android.text.SpannableString
@@ -12,6 +13,12 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.utils.ColorTemplate
 import faks.android.smartexpenses.R
 import faks.android.smartexpenses.databinding.FragmentReportsBinding
 
@@ -31,6 +38,10 @@ class ReportsFragment : Fragment() {
         _binding = FragmentReportsBinding.inflate(inflater, container, false)
 
         setupButtons()
+
+        val pieChart = binding.pieChartCategory.pieChartTemplate
+        setupPieChart(pieChart)
+        loadPieChartData(pieChart)
 
         // Return the root of the binding
         return binding.root
@@ -66,4 +77,40 @@ class ReportsFragment : Fragment() {
             button.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.blue))
         }
     }
+
+    private fun setupPieChart(pieChart: PieChart) {
+        // Configure pie chart appearance
+        pieChart.isDrawHoleEnabled = true
+        pieChart.setUsePercentValues(true)
+        pieChart.setEntryLabelTextSize(12f)
+        pieChart.setEntryLabelColor(Color.BLACK)
+        pieChart.centerText = "Distribution"
+        pieChart.setCenterTextSize(24f)
+        // Configure legend
+        val legend = pieChart.legend
+        legend.verticalAlignment = Legend.LegendVerticalAlignment.TOP
+        legend.horizontalAlignment = Legend.LegendHorizontalAlignment.RIGHT
+        legend.orientation = Legend.LegendOrientation.VERTICAL
+        legend.setDrawInside(false)
+        legend.isEnabled = true
+    }
+
+    private fun loadPieChartData(pieChart: PieChart) {
+        val entries = ArrayList<PieEntry>()
+        // TODO: Replace this with your data
+        entries.add(PieEntry(30.0f, "Category A"))
+        entries.add(PieEntry(20.0f, "Category B"))
+        entries.add(PieEntry(50.0f, "Category C"))
+
+        val dataSet = PieDataSet(entries, "Categories")
+        // TODO: Customize colors
+        dataSet.setColors(*ColorTemplate.MATERIAL_COLORS)
+        dataSet.sliceSpace = 3f
+        dataSet.valueTextSize = 18f
+
+        val data = PieData(dataSet)
+        pieChart.data = data
+        pieChart.invalidate() // refresh
+    }
+
 }
